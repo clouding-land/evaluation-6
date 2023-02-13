ec3e247cbfeda486e5bb7597338a680111479b12
clouding-land <samsitha1210@gmail.com>
2023-02-13 19:31:18 +0530

    evaluation-6

--- evaluation-6/src/components/Sidebar.js
+++ evaluation-6/src/components/Sidebar.js
@@ -21,8 +21,8 @@ function Sidebar() {
         <h4>{data.personal.name}</h4>
         <Link href="https://github.com/clouding-land">@Clouding-land</Link>
         <ul styles ={styles.ul}>
-                {data.personal.desig.map((desigs) =>(
-                    <li className={styles.list}>{desigs} | </li>
+                {data.personal.desig.map((desigs,index) =>(
+                    <li key={index.toString()} className={styles.list}>{desigs} | </li>
                 ))}
         </ul>
         <div className='buttons row'>
@@ -42,8 +42,8 @@ function Sidebar() {
     <div className="row-3">
         <div className={styles.sidebar}>
                 <ul className={styles.ul}>
-                {data.personal.knowledge.map((desigs) =>(
-                    <li className={styles.list1}>{desigs}</li>
+                {data.personal.knowledge.map((desigs,index) =>(
+                    <li key={index.toString()} className={styles.list1}>{desigs}</li>
                 ))}
                 </ul>
         </div>
@@ -52,7 +52,7 @@ function Sidebar() {
         <div className={styles.sidebar}>
                 <ul>
                 {data.experience.map((desigs) =>(
-                    <li className={styles.list2}>
+                    <li key={desigs.id} className={styles.list2}>
                         <h6>{desigs.name}</h6>
                         <p>{desigs.time}</p>
                     </li>
